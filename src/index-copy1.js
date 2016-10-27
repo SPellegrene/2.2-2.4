eaa7d0930d565dfc9926bce677116bbd5b435c07
Sam Pellegrene <pellegrenesam@gmail.com>
2016-10-27 15:27:32 -0500

    done with first question

--- src/index-copy1.js
+++ src/index-copy1.js
@@ -1,24 +1,14 @@
 
-// CHALLENGE : SCROLLING RANDOM NUMBERS
-
-
 // write a function randomInRange(a,b)
 // that returns a random number between a and b
 //
 // note: Math.random() returns a float between 0 and 1
-// a and b are (0,1)
 
-function randomInRange(a,b){
+function randomInRange(a,b) {
 
- var max = Math.max(a,b);
- var min = Math.min (a,b);
-
-  return (Math.random() * (max-min)+min);
-};
+}
 
 var x = randomInRange(5,100)
 console.assert(x >= 5 && x <= 100)
 var y = randomInRange(-25,30)
 console.assert(y >= -25 && y <= 30)
-
-console.log('This passed');
